0ca0066678073f990608194b32dd38dc8f782c3a
Nikola Lukic <nlukic97@gmail.com>
2021-10-24 18:08:22 +0200

    might have made a function to show the progress percentage, still have to figure out the positioning

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,9 @@
 const circumference = document.querySelector('.progress-circle').getTotalLength()
 console.log(circumference);
+
+function setProgress(percent){
+    document.querySelector('.progress-circle').style.strokeDasharray = circumference;
+    document.querySelector('.progress-circle').style.strokeDashoffset = circumference - percent * circumference/100;
+}
+
+setProgress(20)
