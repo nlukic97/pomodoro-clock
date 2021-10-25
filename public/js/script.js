35ee374924ad306192bd63791d0872ec9178f825
Nikola Lukic <nlukic97@gmail.com>
2021-10-25 11:08:54 +0200

    made the circle counter for a countdown based on %

--- public/js/script.js
+++ public/js/script.js
@@ -6,4 +6,21 @@ function setProgress(percent){
     document.querySelector('.progress-circle').style.strokeDashoffset = circumference - percent * circumference/100;
 }
 
-setProgress(20)
+setProgress(50)
+// setProgress(98)
+
+
+
+
+function startCountdown(){
+    let i = 0;
+    let int = setInterval(function(){
+        setProgress(i)
+        if(i >= 100){
+            return clearInterval(int)
+        }
+        i++
+    },100)
+}
+
+// startCountdown()
